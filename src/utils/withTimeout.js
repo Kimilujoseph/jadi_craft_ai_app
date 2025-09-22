d4a65b4f74b2d5f259d99ed273c59b4a111dfb05
timothy joseph <timothyjoseph8580@gmail.com>
2025-09-23 00:26:43 +0300

    solved the import module error at withTimeout.js file

--- src/utils/withTimeout.js
+++ src/utils/withTimeout.js
@@ -1,10 +1,10 @@
-import ErrorHandler from "./ErrorHandler";
+import ApiError from "./errors/ApiError.js";
 //this part can be confusing some but here is an explanation
 //text me i will give more details
 const withTimeout = (fn, timeoutMs) => {
   return (...args) => {
     const timeoutPromise = new Promise((_, reject) => {
-      setTimeout(() => reject(new ErrorHandler("Request Timeout", 504)), timeoutMs);
+      setTimeout(() => reject(new ApiError("Request Timeout", 504)), timeoutMs);
     });
 
     return Promise.race([
